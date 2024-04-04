78d915d8a7c3b2fd17570519d4b7d1054deb2502
GD <GD@DESKTOP-62BCQE1>
2024-04-05 08:45:46 +0900

    git commit -m "chap1"

---
+++ javastudy/src/chap/test/Modifier2.java
@@ -0,0 +1,15 @@
+package chap.test;
+
+public class Modifier2 {
+		private int v1 = 100;
+				int v2 = 200;
+		protected int v3= 300;
+		public int v4 = 400;
+		public void method() {
+			System.out.println("chap7.Modifier 클래스의 메서드");
+			System.out.println("v1="+v1);
+			System.out.println("v2="+v2);
+			System.out.println("v3="+v3);
+			System.out.println("v4="+v4);
+		}
+}
